40ec28a229521eb9166b3b0d028a4249f2ba0f30
tuguzT <timurka.tugushev@gmail.com>
2023-12-07 09:58:28 +0300

    Add unit tests for strings

--- tests/InsertionSort.cpp
+++ tests/InsertionSort.cpp
@@ -4,6 +4,7 @@
 #include <array>
 #include <vector>
 #include <random>
+#include <string>
 
 GTEST_TEST(InsertionSortTest, EmptyArray) {
     std::array<int, 0> array{};
@@ -33,6 +34,19 @@ GTEST_TEST(InsertionSortTest, SortedArray) {
     GTEST_ASSERT_TRUE(std::is_sorted(array.begin(), array.end(), std::greater()));
 }
 
+GTEST_TEST(InsertionSortTest, SortedStringArray) {
+    using namespace std::string_literals;
+
+    std::array array = {"1"s, "2"s, "3"s, "4"s, "5"s, "6"s, "7"s, "8"s, "9"s};
+    GTEST_ASSERT_TRUE(std::is_sorted(array.begin(), array.end(), std::less()));
+
+    lab3::insertion_sort(array.begin(), array.end(), std::less());
+    GTEST_ASSERT_TRUE(std::is_sorted(array.begin(), array.end(), std::less()));
+
+    lab3::insertion_sort(array.begin(), array.end(), std::greater());
+    GTEST_ASSERT_TRUE(std::is_sorted(array.begin(), array.end(), std::greater()));
+}
+
 GTEST_TEST(InsertionSortTest, ReverseSortedArray) {
     std::array array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
     GTEST_ASSERT_TRUE(std::is_sorted(array.begin(), array.end(), std::greater()));
@@ -41,8 +55,28 @@ GTEST_TEST(InsertionSortTest, ReverseSortedArray) {
     GTEST_ASSERT_TRUE(std::is_sorted(array.begin(), array.end(), std::less()));
 }
 
+GTEST_TEST(InsertionSortTest, ReverseSortedStringArray) {
+    using namespace std::string_literals;
+
+    std::array array = {"9"s, "8"s, "7"s, "6"s, "5"s, "4"s, "3"s, "2"s, "1"s};
+    GTEST_ASSERT_TRUE(std::is_sorted(array.begin(), array.end(), std::greater()));
+
+    lab3::insertion_sort(array.begin(), array.end(), std::less());
+    GTEST_ASSERT_TRUE(std::is_sorted(array.begin(), array.end(), std::less()));
+}
+
 GTEST_TEST(InsertionSortTest, RandomArray) {
     std::array array = {8, 3, 5, 1, 0, 2, 6, 4, 7};
+    GTEST_ASSERT_FALSE(std::is_sorted(array.begin(), array.end(), std::less()));
+
+    lab3::insertion_sort(array.begin(), array.end(), std::less());
+    GTEST_ASSERT_TRUE(std::is_sorted(array.begin(), array.end(), std::less()));
+}
+
+GTEST_TEST(InsertionSortTest, RandomStringArray) {
+    using namespace std::string_literals;
+
+    std::array array = {"8"s, "3"s, "5"s, "1"s, "0"s, "2"s, "6"s, "4"s, "7"s};
     GTEST_ASSERT_FALSE(std::is_sorted(array.begin(), array.end(), std::less()));
 
     lab3::insertion_sort(array.begin(), array.end(), std::less());
--- tests/QuickSort.cpp
+++ tests/QuickSort.cpp
@@ -4,6 +4,7 @@
 #include <array>
 #include <vector>
 #include <random>
+#include <string>
 
 GTEST_TEST(QuickSortTest, EmptyArray) {
     std::array<int, 0> array{};
@@ -33,6 +34,19 @@ GTEST_TEST(QuickSortTest, SortedArray) {
     GTEST_ASSERT_TRUE(std::is_sorted(array.begin(), array.end(), std::greater()));
 }
 
+GTEST_TEST(QuickSortTest, SortedStringArray) {
+    using namespace std::string_literals;
+
+    std::array array = {"1"s, "2"s, "3"s, "4"s, "5"s, "6"s, "7"s, "8"s, "9"s};
+    GTEST_ASSERT_TRUE(std::is_sorted(array.begin(), array.end(), std::less()));
+
+    lab3::quick_sort(array.begin(), array.end(), std::less());
+    GTEST_ASSERT_TRUE(std::is_sorted(array.begin(), array.end(), std::less()));
+
+    lab3::quick_sort(array.begin(), array.end(), std::greater());
+    GTEST_ASSERT_TRUE(std::is_sorted(array.begin(), array.end(), std::greater()));
+}
+
 GTEST_TEST(QuickSortTest, ReverseSortedArray) {
     std::array array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
     GTEST_ASSERT_TRUE(std::is_sorted(array.begin(), array.end(), std::greater()));
@@ -41,8 +55,28 @@ GTEST_TEST(QuickSortTest, ReverseSortedArray) {
     GTEST_ASSERT_TRUE(std::is_sorted(array.begin(), array.end(), std::less()));
 }
 
+GTEST_TEST(QuickSortTest, ReverseSortedStringArray) {
+    using namespace std::string_literals;
+
+    std::array array = {"9"s, "8"s, "7"s, "6"s, "5"s, "4"s, "3"s, "2"s, "1"s};
+    GTEST_ASSERT_TRUE(std::is_sorted(array.begin(), array.end(), std::greater()));
+
+    lab3::quick_sort(array.begin(), array.end(), std::less());
+    GTEST_ASSERT_TRUE(std::is_sorted(array.begin(), array.end(), std::less()));
+}
+
 GTEST_TEST(QuickSortTest, RandomArray) {
     std::array array = {8, 3, 5, 1, 0, 2, 6, 4, 7};
+    GTEST_ASSERT_FALSE(std::is_sorted(array.begin(), array.end(), std::less()));
+
+    lab3::quick_sort(array.begin(), array.end(), std::less());
+    GTEST_ASSERT_TRUE(std::is_sorted(array.begin(), array.end(), std::less()));
+}
+
+GTEST_TEST(QuickSortTest, RandomStringArray) {
+    using namespace std::string_literals;
+
+    std::array array = {"8"s, "3"s, "5"s, "1"s, "0"s, "2"s, "6"s, "4"s, "7"s};
     GTEST_ASSERT_FALSE(std::is_sorted(array.begin(), array.end(), std::less()));
 
     lab3::quick_sort(array.begin(), array.end(), std::less());
